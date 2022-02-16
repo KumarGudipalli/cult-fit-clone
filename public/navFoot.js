e231b861402168ceee1dab74b63352badfeb9883
KumarGudipalli <kumargudipalli97@gmail.com>
2022-02-16 09:28:16 +0530

    cloned

---
+++ public/navFoot.js
@@ -0,0 +1,403 @@
+function navbar(){
+  return `<div id="smallScreenNav">
+  <div id="hamburgericon">
+      <div></div>
+      <div></div>
+      <div></div>
+  </div>
+  <div id="dropdownNav">
+      <div id="cultlogodis">
+          <img src="https://static.cure.fit/assets/images/curefit-v-man-white.svg" alt="user-image" class=" style-prefix-2jfr04 efaomd31" loading="lazy" width="40" height="45.45454545454545">
+      </div>
+      <div id="cross">
+          <i class="fas fa-times"></i>
+      </div>
+       <div id="navPages">
+             <div><a href="/index.html">Home</a></div>
+             <div><a href="/cultpage.html">CULT</a></div>
+             <div><a href="/live.html">LIVE</a></div>
+             <div><a href="/care.html">CARE</a></div>
+             <div><a href="/mind.html">MIND</a></div>
+             <div><a href="/storepage.html">STORE</a></div>
+             <div><a href="/eatordernow.html">EAT</a></div>
+            
+       </div>
+       <div id="downLogin">
+          <div>SignIn</div>
+          <div><a href="/cartpage.html">Cart</a></div>
+          <div><a href="/index.html">LogOut</a></div>
+     </div>
+  </div>
+</div>
+<div class="headerDropDown" id="headDropDown">
+  <div class="dropDownElem">
+       <div class="image-wrapper " style="">
+       <a href="cultpage.html">
+         <img alt="" src="https://static.cure.fit/assets/images/cult-icon.svg">
+         </a>
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        CULT
+      </div>
+  </div>
+<div class="dropDownElem">
+       <div class="image-wrapper " style="">
+       <a href="live.html">
+         <img alt="" src="https://static.cure.fit/assets/images/Livefit.svg">
+         </a>
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        LIVE
+      </div>
+  </div>
+<div onclick="window.location.href='C:/Users/pavan/Desktop/MASAI SCHOOL/Projects/Cult Fit clone Project u-3/EAT/eatordernow.html'" class="dropDownElem">
+       <div class="image-wrapper " style="">
+         <a href="eatorder.html">
+           <img alt="" src="https://static.cure.fit/assets/images/eat-icon.svg">
+         </a>
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        EAT
+      </div>
+  </div>
+<div class="dropDownElem">
+       <div class="image-wrapper " style="">
+       <a href="mind.html">
+         <img  alt="" src="https://static.cure.fit/assets/images/mind-icon.svg">
+        </a>
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        MIND
+      </div>
+  </div>
+<div class="dropDownElem">
+       <div class="image-wrapper " style="">
+       <a href="care.html">
+         <img alt="" src="https://static.cure.fit/assets/images/carefit-blk.svg">
+        </a>
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        CARE
+      </div>
+  </div>
+<div class="dropDownElem">
+       <div class="image-wrapper " style="">
+         <img alt="" src="	https://static.cure.fit/assets/images/store-icon-hover.svg">
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        STORE
+      </div>
+  </div>
+</div>
+<div id="innerNavbar">
+  <div id="logo" class="logo">
+      <a href="logout.html">
+          <img src="https://static.cure.fit/assets/images/curefit-v-man.svg" alt="cult fit logo">
+      </a>
+      <div id="spet">
+        <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/image/header-web/store_fit.svg" alt="store" class=" css-130q1oe-ImageElement efaomd31" loading="lazy" width="92.2" height="20">    
+        <img  id="dropdown-arrow" class="dropdownarrow" src="https://static.cure.fit/assets/images/arrow-clp.svg" alt="Expand menu" style="transform: rotate(180deg); min-height: 15px; min-width: 15px;">
+      </div>
+  </div>
+   <div id="eatSubpages">
+       <div>
+         <ul>
+           <li id="order"><a href="menproduct.html" >cultsport</a></li>
+         </ul>
+       </div>
+       
+   </div>
+  <div id="user-details" class="user-details">
+      <div id="location" class="location">
+
+          <div id="blue-dot" class="blue-dot"></div>
+          <div id="cityName" class="cityName">Bangalore</div>
+      </div>
+      <div id="profile">
+      <img src="https://profile-pictures-prod.s3.amazonaws.com/5491b50b-b284-490f-a55d-db97431b1e32" style="max-width:100%;max-height:90%">
+      </div>
+      <div id="logged-out" class="logged-out">
+      <div>
+        <img id="image11" src="https://cdn-icons-png.flaticon.com/512/25/25623.png" />
+      </div>
+     <div id="logindata">
+    
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/profile-icon.svg" />
+              <a class="allat" >Profile</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/cart-black.svg" />
+              <a class="allat" >Orders</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/health-profile-black.svg" />
+              <a class="allat" >Medical Records</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/combined-shape-black.svg" />
+              <a class="allat" >Active Packs</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/coupon-black.svg" />
+              <a class="allat" >Redeem Vouchers</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/settings-black.svg" />
+              <a class="allat" >Account</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/support-black.svg" />
+              <a class="allat" >Support</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/logout-black.svg" />
+              <a class="allat" href="">Logout</a>
+          </div>
+  </div>
+      </div>
+      <div id="cart" class="cart">
+          <img id="cartNik" src="https://static.cure.fit/assets/images/cart-image.svg" alt="cart logo">
+      </div>
+  </div>
+</div>`
+}
+function footer(){
+  return `
+  <div class="top-footer">
+    <div class="image-description">
+      <img src="https://static.cure.fit/assets/images/cult-logo-white.svg" alt="cult logo" width="126px" height="42px">
+      <div class="description">At cult.fit, we make group workouts fun, daily food healthy & tasty, mental fitness easy with yoga & meditation, and medical & lifestyle care hassle-free. #BeBetterEveryDay</div>
+    </div>
+    <ul id="contact-details" class="contact-details">
+      <li><a href="#">CONTACT US</a></li>
+      <li><a href="#">BLOG</a></li>
+      <li><a href="#">PARTNER.FIT</a></li>
+      <li><a href="#">TALKS</a></li>
+      <li><a href="#">CAREERS</a></li>
+    </ul>
+
+    <ul class="cult-fit-bussiness">
+      <li><a href="#"> CULTFIT FOR BUSINESSES</a></li>
+      <li><a href="#">CULT FRANCHISE</a></li>
+      <li><a href="#">CULT PASS NETWORK</a></li>
+      <li><a href="#">CORPORATE PARTNERSHIPS</a></li>
+    
+    </ul>
+    <div class="play-button">
+      <div class="istore-play-btn">
+        <img src="https://static.cure.fit/assets/images/app-store.svg" alt="istore btn" width="133px" height="42px">
+      </div>
+      <div class="play-store-btn">
+        <img src="https://static.cure.fit/assets/images/play-store.svg" alt="google play btn" width="133px" height="42px">
+      </div>
+    </div>
+  </div>
+
+  <div class="middle-footer">
+    <div class="middle-footer-related-search">RELATED SEARCHES</div>
+    <div class="middle-footer-text-div">
+      <div class="middle-footer-text-inner-div">
+        <div class="middle-footer-text">
+          <a href="#">Gym Near Me</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Consult Nutritionist Online</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Exercise To Reduce Belly Fat</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Meditation Music</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Dermatologist online</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Chennai</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Coimbatore</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Mobility Exercise</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Core Exercise</a>
+        </div>
+      </div>
+      <div class="middle-footer-text-inner-div">
+        <div class="middle-footer-text">
+          <a href="#">Online Personal Training</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Consult Gynocologist Online</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Six Pack Workout</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Pranayam</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Orthopedist doctor near me</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Jaipur</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Bangalore</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Kids Exercise</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Butt Exercise</a>
+        </div>
+      </div>
+      <div class="middle-footer-text-inner-div">
+        <div class="middle-footer-text">
+          <a href="#">Online Doctor Consultation</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Diagonistc Tests</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Yoga Postures for Begginer</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Bedtime Stories For Kids</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Mumbai</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Kolkata</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Home Workouts</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Dance Exercise</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Arm workout</a>
+        </div>
+      </div>
+      <div class="middle-footer-text-inner-div">
+        <div class="middle-footer-text">
+          <a href="#">Therapist consultation</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Food Near Me</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Surya Namaskar for Begginers</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Meditation in Hindi</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Delhi</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Ahmedabad</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Belly fat Loss Exercise</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">How To Lose Weight</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Back Exercise</a>
+        </div>
+      </div>
+      <div class="middle-footer-text-inner-div">
+        <div class="middle-footer-text">
+          <a href="#">Online Fitness Classes</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Healthy Receipes</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Yoga for Strss Relief</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Mindfullness Meditation</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Pune</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Chandigarh</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Weight Loss Exercise</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">HIIT Workout</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Abs Workout</a>
+        </div>
+      </div>
+      <div class="middle-footer-text-inner-div">
+        <div class="middle-footer-text">
+          <a href="#">Consult Cardiologist Online</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Chest Exercise</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Yoga Asanas</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Yoga Nidra</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Hyderbad</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Gyms in Indore</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Strength Training</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Exercise for Toned Body</a>
+        </div>
+        <div class="middle-footer-text">
+          <a href="#">Thigh Exercise</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="lower-footer-div" class="lower-footer-div">
+    <div class="lower-footer-div-text-div">
+      <ul class="lower-footer-div-text">
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Terms & conditions</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Refund Policy</a></li>
+      </ul>
+    </div>
+    <div class="lower-footer-social-icons">
+      <a href="#">
+        <img src="https://static.cure.fit/assets/images/facebook_icon_white.svg" alt="facebook icon">
+      </a>
+      <a href="#">
+        <img src="https://static.cure.fit/assets/images/twitter_icon_white.svg" alt="Twitter Icon">
+      </a>
+      <a href="#">
+        <img src="https://static.cure.fit/assets/images/instagram_icon_white.svg" alt="Instagram icon">
+      </a>
+      <a href="#">
+        <img src="https://static.cure.fit/assets/images/youtube_icon_white_1.svg" alt="Youtube icon">
+      </a>
+    </div>
+  </div>`
+}
+export {navbar,footer}
